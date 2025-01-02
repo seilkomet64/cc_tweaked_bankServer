8e6ff6912d3311f450e4aaf87b496443b51f0b7b
seilkomet64 <seilkomet64@gmail.com>
2025-01-02 03:11:33 +0100

    Clear the terminal after booting

--- src/autorun/startup.lua
+++ src/autorun/startup.lua
@@ -2,5 +2,6 @@ require(settings.get("ghu.base") .. "core/apis/ghu")
 local bankController = require("bankController")
 require("config")
 
+term.clear()
 print("Welcome to BankOS")
 bankController.StartBankingSystem()

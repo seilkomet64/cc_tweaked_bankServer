43a2e7cb0dbd33cf83080d03c2f6abe0433de860
seilkomet64 <seilkomet64@gmail.com>
2025-01-04 03:20:43 +0100

    fix parallel

--- src/autorun/startup.lua
+++ src/autorun/startup.lua
@@ -8,4 +8,4 @@ term.setCursorPos(1, 1)
 print("Welcome to BankOS")
 -- Start the banking system and check for pending transactions
 -- Pending Transactions is an infinite loop that checks for transactions that have not been confirmed and will not finish 
-parallel.waitForAny(bankController.StartBankingSystem, bankAPI.checkPendingTransactions())
+parallel.waitForAny(bankController.StartBankingSystem, bankAPI.checkPendingTransactions)

934b646a3371c3a983a34c1827e679fdab2a5030
seilkomet64 <seilkomet64@gmail.com>
2025-01-04 03:04:48 +0100

    reset cursor position after clear

--- src/autorun/startup.lua
+++ src/autorun/startup.lua
@@ -4,6 +4,7 @@ local bankAPI = require("bankAPI")
 require("config")
 
 term.clear()
+term.setCursorPos(1, 1)
 print("Welcome to BankOS")
 -- Start the banking system and check for pending transactions
 -- Pending Transactions is an infinite loop that checks for transactions that have not been confirmed and will not finish 
